7bf3cc449a1b687ac256efafd04d49397796f5ff
noobmaster69-spec <remotfanatha8@gmail.com>
2025-09-17 20:16:53 +0700

    Praktikum pertama

---
+++ PraktikumAlprokom1.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikumalprokom1;
+
+/**
+ *
+ * @author Other user
+ */
+public class PraktikumAlprokom1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      // TODO code application logic here
+      int umur = 18;
+      double tinggi = 190.22;
+      float angka = 170.22222f ;
+      boolean status = false;
+      double desimal = 345.99999d;
+      int penjumlahan = umur + 100;
+      float perkalian = angka * 10;
+      
+        System.out.println(umur);
+        System.out.print("remo ganteng ");
+        System.out.println("anjai");
+        System.out.println(angka);
+        System.out.println(status);
+        System.out.println(desimal);
+        System.out.print("penjumlahan umur = ");
+        System.out.println(penjumlahan);
+        System.out.println(perkalian);
+        
+        
+    }
+    
+}
